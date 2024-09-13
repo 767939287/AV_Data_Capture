10cccec8fbb4ab31e1b9c98074d4fe711ea0d855
桜咲 <767939287@qq.com>
2024-09-13 21:33:37 +0800

    fix

--- scrapinglib/httprequest.py
+++ scrapinglib/httprequest.py
@@ -8,7 +8,7 @@ from cloudscraper import create_scraper
 
 import config
 
-G_USER_AGENT = r' (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.133 Safari/537.36'
+G_USER_AGENT = r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.133 Safari/537.36'
 G_DEFAULT_TIMEOUT = 10
 
 
