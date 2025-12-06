f89a0db04300abc17e7242683c732c11f82edb54
桜咲 <767939287@qq.com>
2025-12-06 08:28:13 +0800

    1

--- scrapinglib/storyline.py
+++ scrapinglib/storyline.py
@@ -229,7 +229,7 @@ def getStoryline_avno1(number, debug, proxies, verify):  # 获取剧情介绍 �
             page_number = title[title.rfind(' ')+1:].strip()
             if not partial_num:
                 # 不选择title中带破坏版和破坏版的简介
-                 if re.match(f'^{number}$', page_number, re.I) and not re.search(r'破解版|破坏版|破壞版|马赛克破解版|馬賽克破解版', title):
+                 if re.match(f'^{number}$', page_number, re.I) and title.rfind('破解版') == -1 and title.rfind('破坏版') == -1 and title.rfind('破解版') == -1:
                     return desc.strip()
             elif re.search(number, page_number, re.I):
                 return desc.strip()
